125f58e1f2d30262737d46b44dc93465dbba082e
juansmolano <juansmolano@gmail.com>
2018-03-27 23:47:01 -0500

    keycloak test

--- frontend/emi/profile/profile.component.ts
+++ frontend/emi/profile/profile.component.ts
@@ -1,22 +1,24 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { fuseAnimations } from '../../../core/animations';
+import { KeycloakService } from 'keycloak-angular';
+import { KeycloakProfile } from 'keycloak-js';
 
 @Component({
-    selector     : 'profile',
-    templateUrl  : './profile.component.html',
-    styleUrls    : ['./profile.component.scss'],
+    selector: 'profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.scss'],
     encapsulation: ViewEncapsulation.None,
-    animations   : fuseAnimations
+    animations: fuseAnimations
 })
-export class ProfileComponent implements OnInit
-{
+export class ProfileComponent implements OnInit {
 
-    constructor()
-    {
+    userDetails: KeycloakProfile;
+
+    constructor(private keycloakService: KeycloakService) {
 
     }
 
-    ngOnInit()
-    {
+    async ngOnInit() {
+        this.userDetails = await this.keycloakService.loadUserProfile();
     }
 }
