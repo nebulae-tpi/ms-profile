95c4f6fd7abfdb0c7cedc7a4649fb85e593f1a11
Daniel Tobon <daniel.tobon@nebulae.com.co>
2018-04-02 18:18:37 -0500

    Se agrega archivos de internacionalizacion

---
+++ frontend/emi/profile/i18n/en.ts
@@ -0,0 +1,20 @@
+export const locale = {
+  lang: 'en',
+  data: {
+      'PROFILE': {
+        'GENERAL_INFORMATION': 'General information',
+        'ROLES': 'Roles',
+        'NAME': 'Name',
+        'LASTNAME': 'Lastname',
+        'EMAIL': 'Email',
+        'USER': 'User',
+        'PASSWORD': 'Password',
+        'OPERATOR': 'Operator',
+        'DASHBOARD': 'Dashboard',
+        'PROFILE': 'Profile',
+        'LOGIN_AT': 'Login at',
+        'LATEST_ACTIVITY': 'Latest activity',
+        'ABOUT': 'About'
+      }
+  }
+};
---
+++ frontend/emi/profile/i18n/es.ts
@@ -0,0 +1,20 @@
+export const locale = {
+  lang: 'es',
+  data: {
+    'PROFILE': {
+      'GENERAL_INFORMATION': 'Información general',
+      'ROLES': 'Roles',
+      'NAME': 'Nombre',
+      'LASTNAME': 'Apellido',
+      'EMAIL': 'Email',
+      'USER': 'Usuario',
+      'PASSWORD': 'Contraseña',
+      'OPERATOR': 'Operador',
+      'DASHBOARD': 'Tablero',
+      'PROFILE': 'Perfil',
+      'LOGIN_AT': 'Inicio de sesión',
+      'LATEST_ACTIVITY': 'Ultimas actividades',
+      'ABOUT': 'Acerca de'
+    }
+  }
+};
