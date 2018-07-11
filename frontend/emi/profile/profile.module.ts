d1929501d8fb9ebdaf9ddb1d3888237ed550119f
Felipe Santa <juan.santa@nebulae.com.co>
2018-07-11 16:59:34 -0500

    frontend modified to test the frontend composer in circleCi workflow

--- frontend/emi/profile/profile.module.ts
+++ frontend/emi/profile/profile.module.ts
@@ -1,38 +1,31 @@
-import { NgModule } from '@angular/core';
-import { SharedModule } from '../../../core/modules/shared.module';
-import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { SharedModule } from "../../../core/modules/shared.module";
+import { RouterModule } from "@angular/router";
 
-import { ProfileComponent } from './profile.component';
-import { ProfileTimelineComponent } from './tabs/timeline/timeline.component';
-import { ProfileAboutComponent } from './tabs/about/about.component';
-import { ProfilePhotosVideosComponent } from './tabs/photos-videos/photos-videos.component';
-import { ProfileService } from './profile.service';
+import { ProfileComponent } from "./profile.component";
+import { ProfileTimelineComponent } from "./tabs/timeline/timeline.component";
+import { ProfileAboutComponent } from "./tabs/about/about.component";
+import { ProfilePhotosVideosComponent } from "./tabs/photos-videos/photos-videos.component";
+import { ProfileService } from "./profile.service";
 
 const routes = [
-    {
-        path     : '',
-        component: ProfileComponent,
-        resolve  : {
-            profile: ProfileService
-        }
+  {
+    path: "",
+    component: ProfileComponent,
+    resolve: {
+      profile: ProfileService
     }
+  }
 ];
 
 @NgModule({
-    declarations: [
-        ProfileComponent,
-        ProfileTimelineComponent,
-        ProfileAboutComponent,
-        ProfilePhotosVideosComponent
-    ],
-    imports     : [
-        SharedModule,
-        RouterModule.forChild(routes)
-    ],
-    providers   : [
-      ProfileService
-    ]
+  declarations: [
+    ProfileComponent,
+    ProfileTimelineComponent,
+    ProfileAboutComponent,
+    ProfilePhotosVideosComponent
+  ],
+  imports: [SharedModule, RouterModule.forChild(routes)],
+  providers: [ProfileService]
 })
-export class ProfileModule
-{
-}
+export class ProfileModule {}
